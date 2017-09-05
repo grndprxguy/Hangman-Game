2136fa272ee72b780151ba2be3893db260f58f4c
grndprxguy <matthew.nagy@me.com>
2017-09-05 17:46:55 -0400

    progress

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,31 @@
+// word bank
+var words =["ACADIA","BADLANDS","DENALI","EVERGLADES","GLACIER","OLYMPIC","PINNACLES","REDWOOD","YELLOWSTONE","YOSEMITE","ZION"]
+
+// set variables
+var wins = 0;
+var guessRemain = 15;
+
+// pick a word from the word bank
+var gameWord = words[Math.floor(Math.random() * words.length)];
+
+// Set Array from gameWord
+var wordArray = new Array(gameWord.length);
+
+// Set _ for letter in wordArray
+// for (var i = 0; i < gameWord.length; i++){
+// 	wordArray[i] = "_ ";
+// }
+
+
+console.log(gameWord)
+
+var charsTyped = [];
+var end = 0;
+
+document.onkeypress = function(evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+    charStr = charStr.toUpperCase()
+    console.log(gameWord.includes(charStr))
+};
