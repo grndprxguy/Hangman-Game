0be9c1e13380542319fe6bf926c82c0de4b87579
grndprxguy <matthew.nagy@me.com>
2017-09-08 07:53:10 -0400

    slow progress

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -7,25 +7,41 @@ var guessRemain = 15;
 
 // pick a word from the word bank
 var gameWord = words[Math.floor(Math.random() * words.length)];
-
 // Set Array from gameWord
 var wordArray = new Array(gameWord.length);
-
 // Set _ for letter in wordArray
-// for (var i = 0; i < gameWord.length; i++){
-// 	wordArray[i] = "_ ";
-// }
+for (var i = 0; i < gameWord.length; i++){
+	wordArray[i] = "_ ";
+}
 
-
-console.log(gameWord)
 
 var charsTyped = [];
-var end = 0;
-
+var correctGuess = [];
+// check keypress against gameWord
 document.onkeypress = function(evt) {
     evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
     var charStr = String.fromCharCode(charCode);
-    charStr = charStr.toUpperCase()
-    console.log(gameWord.includes(charStr))
-};
+    charStr = charStr.toUpperCase();
+    charsTyped.push(charStr);
+    var tryCount = function(){tryCount++};
+    
+
+  if (gameWord.includes(charStr) == true) {
+  	correctGuess.push(charStr);
+  }
+  // checking results
+  console.log(charsTyped);
+  console.log(correctGuess);
+  console.log("-----");
+
+  var tryCount = 0;}
+
+  document.getElementByID("divtry").innerHTML=tryCount;
+
+
+
+ 
+
+
+
